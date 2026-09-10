b8c595f68f0168b448a348f09631c7b736581e08
Şamil Bozkurt <samilbozkurt27@gmail.com>
2026-09-10 13:49:13 +0300

    Practice python operators and user input

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ print(type(10))
 print(type(10.5))
 print(type("Merhaba"))
 print(type(True))
+
+name = input("Adınız: ")
+weekly_hours = float(input("Haftalık çalışma süreniz: "))
+completed_lessons = int(input("Tamamlanan ders sayısı: "))
+
+monthly_hours = weekly_hours * 4
+
+print(f"Öğrenci: {name}")
+print(f"Haftalık çalışma: {weekly_hours} saat")
+print(f"Aylık tahmini çalışma: {monthly_hours} saat")
+print(f"Tamamlanan ders: {completed_lessons}")
