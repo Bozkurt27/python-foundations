d3c14881ed76d1a6e0496c182cc948682fe9c3b9
Şamil Bozkurt <samilbozkurt27@gmail.com>
2026-09-14 14:51:23 +0300

    practice python file handling

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
-numbers = [10, 20, 30]
+from pathlib import Path
 
-total = 0
+data_folder = Path("data")
+print(data_folder)
+print(type(data_folder))
 
-for number in numbers:
-    total += number
+file_path = Path("data") / "sales.txt"
 
-average = total / len(numbers)
+print(file_path)
 
-print(average)
+
+current_folder = Path.cwd()
+print(current_folder)
+
+
+file = open("data/study_notes.txt", "r", encoding = "utf-8")
+content = file.read()
+print(content)
+file.close()
+
+
+
+with open("data/study_notes.txt", "r", encoding = "utf-8") as file:
+    for line in file:
+        print(line.strip())
+
+output_path = Path("data") / "result.txt"
+with open(output_path, "w", encoding = "utf-8") as file:
+    file.write("İlk dosyam")
+
+with open(output_path, "a", encoding = "utf-8") as file:
+    file.write("\nikinci satır")
+
+
+file_path = Path("data") / "study_notes.txt"
+print(file_path.exists())
+
+
+    
