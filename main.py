ee7c9673a35b2bffcdee92b33654e5d08ae3e587
Şamil Bozkurt <samilbozkurt27@gmail.com>
2026-09-14 13:40:14 +0300

    practice python error handling and debugging

--- main.py
+++ main.py
@@ -1,44 +1,10 @@
-skills = ["Python", "SQL", "Git", "R"]
+numbers = [10, 20, 30]
 
-for index, skill in enumerate(skills):
-    print(index, skill)
+total = 0
 
+for number in numbers:
+    total += number
 
-names = ["Ali", "Ayşe", "Mehmet"]
-scores = [75, 90, 82]
+average = total / len(numbers)
 
-for name, score in zip(names, scores):
-    print(name, score)
-
-models = ["Logistic Regression", "Random Forest", "XGBoost"]
-scores = [0.82, 0.89, 0.91]
-
-for model, score in zip(models, scores):
-    print(f"{model}: {score:.2%}")
-
-
-model_scores = dict(zip(models, scores))
-
-print(model_scores)
-
-numbers = [1, 2, 3, 4, 5]
-squares = [number **2 for number in numbers]
-even_number = [number for number in numbers if number % 2 == 0]
-print(even_number)
-print(squares)
-
-
-skills = ["Python", "SQL", "Git"]
-skill_lengths = {skill: len(skill) for skill in skills}
-print(skill_lengths)
-
-
-scores = [40, 75, 90]
-results = ["Passed" if score > 50 else "Failed" for score in scores]
-print(results)
-
-
-
-
-
-
+print(average)
